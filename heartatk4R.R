b3ba12a1045d1f3b70e97821c50372dadd61c214
alvinkim01 <alvinkim01@gmail.com>
2023-06-25 16:10:26 +0900

    second commit

--- heartatk4R.R
+++ heartatk4R.R
@@ -7,4 +7,3 @@ heartatk4R <- read.table(URL,
 x <- heartatk4R # Make a copy of the data for manipulation, call it x. 
 str(x) # structure of data object, data types for each column
 attach(x) #attach : 데이터를 R 검색 경로에 추가하여 변수명으로 바로 접근할 수 있게 한다.
-barplot(table(DIAGNOSIS))
